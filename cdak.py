454bafee366429aff505fa41560bbf5ddd9f3209
Andrea Argouarch <aargouarch@gmail.com>
2016-08-07 14:30:22 -0700

    added config parser

--- cdak.py
+++ cdak.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
 import csv
 import argparse
+import re
+
+class ConfigParser:
+   def __init__(self, config_file):
+      self.config_file = config_file
+
+   def get_groups(self):
+      groups = [[]]
+      group = 0
+
+      with open(self.config_file, 'r') as configs:
+         for line in configs:
+            if re.match(r'\s*\n', line):
+               group += 1
+               groups.append([])
+            elif re.match(r'\s*#', line):
+               continue
+
+            pair = re.split(r'\s*,\s*', line)
+            if len(pair) == 2:
+               groups[group].append((pair[0], pair[1].rstrip()))
+      return groups
 
 class CDAK:
    @staticmethod
@@ -23,12 +45,13 @@ class CDAK:
          mdim.append([row[str(col)] for col in range(1, 13)])
       return mdim
 
-   def __init__(self, cdak_file):
+   def __init__(self, cdak_file, config_file):
       self.name = cdak_file
       self.file = open(self.name, 'r')
       self.csvreader = csv.DictReader(self.file)
       self.cursor = None
       self.blk = []
+      self.configs = ConfigParser(config_file).get_groups()
 
    def __enter__(self):
       return self
@@ -54,11 +77,14 @@ class CDAK:
 
 def main():
    parser = argparse.ArgumentParser(description="Cathepsin D Assay Kinetics data converter")
+   parser.add_argument('-f', '--config-file', required=True, help='path to config file')
    parser.add_argument('CSV', help='path to csv file to parse')
+   args = parser.parse_args()
 
-   with CDAK(parser.parse_args().CSV) as cdak:
+   with CDAK(args.CSV, args.config_file) as cdak:
       for line in cdak:
          print(line)
+
 
 if __name__ == '__main__':
    main()
