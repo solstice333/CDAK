1eb26ba622198a2ce5fe75475d4d45b4447206f3
Andrea Argouarch <aargouarch@gmail.com>
2016-08-07 02:27:25 -0700

    got blank

--- cdak.py
+++ cdak.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python
 import csv
 import argparse
+import math
 
 class CDAK:
+   @staticmethod
+   def _is_all_values_are_empty(line):
+      return ''.join([s for s in line.values()]) == ''
+
+   @staticmethod
+   def _get_blank(blk):
+      blanks = [float(blk[0]['10']), float(blk[0]['11']), float(blk[0]['12'])]
+      return float(sum(blanks)/len(blanks))
+
    def __init__(self, cdak_file):
       self.name = cdak_file
       self.file = open(self.name, 'r')
+      self.csvreader = csv.DictReader(self.file)
+      self.cursor = None
+      self.blk = []
 
    def __enter__(self):
-      return self.file
+      return self
 
    def __iter__(self):
-      return self.file
+      self.cursor = iter(self.csvreader)
+      return self
 
    def __next__(self):
-      # put everything in here
-      yield self.file.readline()
+      self.blk = []
+
+      for line in self.cursor:
+         if CDAK._is_all_values_are_empty(line):
+            return CDAK._get_blank(self.blk)
+         self.blk.append(line)
+      raise StopIteration
 
    def __exit__(self, exc_type, exc_value, traceback):
       self.file.close()
