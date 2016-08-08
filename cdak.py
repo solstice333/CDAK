ba416cc6f11337b0cb35df14c8c18518f77006f3
Andrea Argouarch <aargouarch@gmail.com>
2016-08-08 02:30:34 -0700

    mainly ConfigParser refactors
    ConfigParser: returns a dict of filename to group now
    CDAK: ctor takes in group directly now
    main:
       - config file argument is -c now instead of -f
       - help desc improved for CSV arg
       - iterate over filename, group outside of CDAK

--- cdak.py
+++ cdak.py
@@ -2,34 +2,38 @@
 import csv
 import argparse
 import re
+import sys
 
 class ConfigParser:
    def __init__(self, config_file):
       self.config_file = config_file
 
    def get_groups(self):
-      groups = [[]]
-      group = 0
+      groups = {}
+      filename = ''
 
       with open(self.config_file, 'r') as configs:
          for line in configs:
-            if re.match(r'\s*\n', line):
-               group += 1
-               groups.append([])
-            elif re.match(r'\s*#', line):
+            if re.match(r'\s*#|\s*\n', line):
                continue
             else:
                if re.match(r'\s*None\s*', line, re.I):
                   node = None
                else:
+                  m = re.match(r'(\"|\')(.*)\1\s*$', line)
+                  if m:
+                     filename = m.group(2)
+                     groups[filename] = []
+                     continue
+
                   pair = re.split(r'\s*,\s*', line)
                   if len(pair) != 2:
                      raise ValueError(
                         "Error: Bad value '{}' in config file".format(line.rstrip()))
                   node = (int(pair[0]), int(pair[1].rstrip()))
-               groups[group].append(node)
 
-      return [l for l in groups if len(l) != 0]
+               groups[filename].append(node)
+      return groups
 
 class CDAK:
    @staticmethod
@@ -75,13 +79,13 @@ class CDAK:
       def __str__(self):
          return ','.join(self.get_diff())
 
-   def __init__(self, cdak_file, config_file):
+   def __init__(self, cdak_file, group):
       self.name = cdak_file
       self.file = open(self.name, 'r')
       self.csvreader = csv.DictReader(self.file)
       self.cursor = None
       self.blk = []
-      self.configs = ConfigParser(config_file).get_groups()
+      self.group = group
 
    def __enter__(self):
       return self
@@ -97,8 +101,7 @@ class CDAK:
          if CDAK._is_all_values_are_empty(line):
             mdim = CDAK._convert_blk_to_mdim(self.blk)
             avg = CDAK._get_blank(self.blk)
-            fibro = CDAK.Fibro(self.configs[0], mdim, avg)
-            return fibro
+            return CDAK.Fibro(self.group, mdim, avg)
          elif CDAK._is_eof(line):
             break
          self.blk.append(line)
@@ -109,14 +112,32 @@ class CDAK:
 
 def main():
    parser = argparse.ArgumentParser(description="Cathepsin D Assay Kinetics data converter")
-   parser.add_argument('-f', '--config-file', required=True, help='path to config file')
-   parser.add_argument('CSV', help='path to csv file to parse')
+   parser.add_argument('-c', '--config-file', required=True, help='path to config file')
+   parser.add_argument('CSV', help='path to input csv file to parse')
    args = parser.parse_args()
 
-   with CDAK(args.CSV, args.config_file) as cdak:
-      for line in cdak:
-         print(line)
+   configs = ConfigParser(args.config_file).get_groups()
 
+   for filename, group in configs.items():
+      if re.match(r'\s*stdout\s*$', filename, re.I):
+         fh = sys.stdout
+         closable = False
+      else:
+         fh = open(filename, 'w')
+         closable = True
+
+      with CDAK(args.CSV, group) as cdak:
+         for line in cdak:
+            fh.write("{}\n".format(line))
+
+      if closable:
+         fh.close()
 
 if __name__ == '__main__':
    main()
+
+
+
+
+
+
